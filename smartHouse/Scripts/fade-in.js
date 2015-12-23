84effbfa5b2393d29f0a462a3533eaf9e9bdb16e
Olexandr Kushnir <olexandrrazor@mail.ru>
2015-12-22 23:20:11 -0700

    firstCommit

---
+++ smartHouse/Scripts/fade-in.js
@@ -0,0 +1,9 @@
+﻿function mem () {
+    //$("#Div1").fadeOut('slow');
+    //$("#Div2").css('background-color', 'green');
+
+    $('#Div1').animate({ opacity: 0.25 }, 2000, function () {
+    });
+
+
+};
